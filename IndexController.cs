5c266c185000007a710eccbb13b1faab29ab60b3
dt9150813 <eddy0712@gmail.com>
2018-10-09 20:39:19 -0600

    Add files via upload

---
+++ IndexController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructuresAssignment2._0.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Stack()
+        {
+            return View();
+        }
+
+        public ActionResult Queue()
+        {
+            return View();
+        }
+
+        public ActionResult Dictionary()
+        {
+            return View();
+        }
+
+        public ActionResult Exit()
+        {
+            return Redirect("https://byu.edu");
+        }
+    }
+}
