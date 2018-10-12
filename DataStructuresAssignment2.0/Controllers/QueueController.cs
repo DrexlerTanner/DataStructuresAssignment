b0112fbdffcf7314e0ff6526d3c96c48bb5b4694
DrexlerTanner <drexlert10@gmail.com>
2018-10-11 18:01:39 -0600

    asdf

--- DataStructuresAssignment2.0/Controllers/QueueController.cs
+++ DataStructuresAssignment2.0/Controllers/QueueController.cs
@@ -9,17 +9,74 @@ namespace DataStructuresAssignment2._0.Controllers
     public class QueueController : Controller
     {
         static Queue<string> myQueue = new Queue<string>();
+        static bool bDisplay = false;
         // GET: Queue
         public ActionResult Index()
         {
-            ViewBag.Berries = myQueue;
+            ViewBag.QueueDisplay = bDisplay;
+            ViewBag.MyQueue = myQueue;
             return View();
         }
+
         public ActionResult AddOne()
         {
-            myQueue.Enqueue("New Entry " + myQueue.Count + 1);
-            ViewBag.Berries = myQueue;
+            myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
+            ViewBag.QueueDisplay = bDisplay;
+            ViewBag.MyQueue = myQueue;
             return View("Index");
+        }
+
+        public ActionResult AddMany()
+        {
+            myQueue.Clear();
+            while (myQueue.Count <= 2000)
+            {
+                myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
+            }
+            ViewBag.QueueDisplay = bDisplay;
+            ViewBag.MyQueue = myQueue;
+            return View("Index");
+        }
+
+        public ActionResult Display()
+        {
+            bDisplay = true;
+            ViewBag.QueueDisplay = bDisplay;
+            ViewBag.MyQueue = myQueue;
+            return View("Index");
+        }
+
+        public ActionResult Delete()
+        {
+            ViewBag.QueueDisplay = bDisplay;
+            if (myQueue.Count == 0)
+            {
+                ViewBag.DeleteError = "There's nothing to be deleted :<";
+            }
+            else
+            {
+                myQueue.Dequeue();
+                ViewBag.MyQueue = myQueue;
+            }
+            return View("Index");
+        }
+
+        public ActionResult Clear()
+        {
+            bDisplay = false;
+            myQueue.Clear();
+            ViewBag.QueueDisplay = bDisplay;
+            return View("Index");
+        }
+
+        public ActionResult Search()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Exit()
+        {
+            return RedirectToAction("Index", "Home");
         }
     }
 }
